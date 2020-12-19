60ccf4f3e6e093392846bfda9bbfa25574aa6733
Nekika <hugo.jahnke@gmail.com>
2020-12-19 22:28:36 +0100

    Imported Spending module within App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { PSpendingModule } from './modules/pspending/pspending.module';
+import { SpendingModule } from './modules/spending/spending.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
       PSpendingModule,
+      SpendingModule,
       UserModule
   ],
   controllers: [],
